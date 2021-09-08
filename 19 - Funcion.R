eb0577b4aa67a9cb85d2b025f8000bd479f7a257
jairvilla <jairasir82@gmail.com>
2021-09-07 19:48:54 -0500

    function

---
+++ 19 - Funcion.R
@@ -0,0 +1,10 @@
+
+# Crear una funcion
+
+calculo_area <- function(width, height) {
+  return(width * height)
+}
+
+resultado <- calculo_area(100, 50)  # se usa la fucion y se especifican los 
+                                    # argumentos de entradas
+resultado
